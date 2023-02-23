b01fdca64a2f93aa3438a4d01647a4773c6c016f
Assem Salama <assem.salama@icloud.com>
2023-02-23 11:18:28 +0200

    Update Generic Entity
    Done

--- src/main/java/com/example/warehouseManager/Model/Generic/GenericEntity.java
+++ src/main/java/com/example/warehouseManager/Model/Generic/GenericEntity.java
@@ -1,10 +1,6 @@
 package com.example.warehouseManager.Model.Generic;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @MappedSuperclass
 public class GenericEntity<T> {
